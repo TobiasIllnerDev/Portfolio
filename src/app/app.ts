0124b851477219036545bc481c10539fbeb18de7
Tobias Illner <140509788+BlueShadow07@users.noreply.github.com>
2026-07-28 17:22:33 +0200

    feat: added structure for html

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,10 +2,11 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Header } from './layout/header/header';
 import { Hero } from './pages/main-section/hero/hero';
+import { Aboutme } from './pages/main-section/aboutme/aboutme';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,Header,Hero],
+  imports: [RouterOutlet,Header,Hero, Aboutme],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
