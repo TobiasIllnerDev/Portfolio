eba02a503c074306f535fa7f26f595ac66dec08b
Tobias Illner <140509788+BlueShadow07@users.noreply.github.com>
2026-07-28 22:35:27 +0200

    feat: added array for icons

--- src/app/pages/main-section/skillset/skillset.ts
+++ src/app/pages/main-section/skillset/skillset.ts
@@ -6,4 +6,55 @@ import { Component } from '@angular/core';
   templateUrl: './skillset.html',
   styleUrl: './skillset.scss',
 })
-export class Skillset {}
+export class Skillset {
+  icons = [
+    {
+      name:'HTML',
+      img:'icons/html.svg'
+    },
+    {
+      name:'CSS',
+      img:'icons/css.svg'
+    },
+    {
+      name:'JavaScript',
+      img:'icons/javascript.svg'
+    },
+    {
+      name:'TypeScript',
+      img:'icons/typescript.svg'
+    },
+    {
+      name:'Angular',
+      img:'icons/angular.svg'
+    },
+    {
+      name:'Git',
+      img:'icons/git.svg'
+    },
+    {
+      name:'SQL',
+      img:'icons/SQL.svg'
+    },
+    {
+      name:'Firebase',
+      img:'icons/Firebase.png'
+    },
+    {
+      name:'React',
+      img:'icons/React.png'
+    },
+    {
+      name:'Python',
+      img:'icons/Python.png'
+    },
+    {
+      name:'Django',
+      img:'icons/Django.png'
+    },
+    {
+      name:'C#',
+      img:'icons/c#-logo.png'
+    }
+  ]
+}
