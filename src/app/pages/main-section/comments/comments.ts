a2c61804682ddcde0ca3a10203d2408e5f5a98a7
Tobias Illner <140509788+BlueShadow07@users.noreply.github.com>
2026-07-31 14:42:48 +0200

    style: styling comments section

--- src/app/pages/main-section/comments/comments.ts
+++ src/app/pages/main-section/comments/comments.ts
@@ -36,18 +36,70 @@ export class Comments {
   ];
 
   activeIndex = 0;
+  trackIndex = this.comments.length > 1 ? 1 : 0;
+  isAnimating = false;
+  isTransitionEnabled = true;
+
+  get sliderComments(): Comment[] {
+    if (this.comments.length <= 1) {
+      return this.comments;
+    }
+
+    const firstComment = this.comments[0];
+    const lastComment = this.comments[this.comments.length - 1];
+
+    return [lastComment, ...this.comments, firstComment];
+  }
 
   showPreviousComment(): void {
+    if (this.comments.length <= 1 || this.isAnimating) {
+      return;
+    }
+
+    this.isAnimating = true;
+    this.isTransitionEnabled = true;
+    this.trackIndex--;
     this.activeIndex =
       (this.activeIndex - 1 + this.comments.length) % this.comments.length;
   }
 
   showNextComment(): void {
-    this.activeIndex =
-      (this.activeIndex + 1) % this.comments.length;
+    if (this.comments.length <= 1 || this.isAnimating) {
+      return;
+    }
+
+    this.isAnimating = true;
+    this.isTransitionEnabled = true;
+    this.trackIndex++;
+    this.activeIndex = (this.activeIndex + 1) % this.comments.length;
   }
 
   showComment(index: number): void {
+    if (this.isAnimating || index === this.activeIndex) {
+      return;
+    }
+
+    this.isAnimating = true;
+    this.isTransitionEnabled = true;
     this.activeIndex = index;
+    this.trackIndex = index + 1;
+  }
+
+  handleTransitionEnd(event: TransitionEvent): void {
+    if (event.target !== event.currentTarget || event.propertyName !== 'transform') {
+      return;
+    }
+
+    const lastTrackIndex = this.comments.length + 1;
+
+    if (this.trackIndex === 0) {
+      this.isTransitionEnabled = false;
+      this.trackIndex = this.comments.length;
+    } else if (this.trackIndex === lastTrackIndex) {
+      this.isTransitionEnabled = false;
+      this.trackIndex = 1;
+    }
+
+    this.isAnimating = false;
   }
 }
