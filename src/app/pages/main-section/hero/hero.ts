e7670d6c0ab10b3bc786e37dd6467ef4ac539065
Tobias Illner <140509788+BlueShadow07@users.noreply.github.com>
2026-07-31 15:02:23 +0200

    styling: herosection text

--- src/app/pages/main-section/hero/hero.ts
+++ src/app/pages/main-section/hero/hero.ts
@@ -6,4 +6,14 @@ import { Component } from '@angular/core';
   templateUrl: './hero.html',
   styleUrl: './hero.scss',
 })
-export class Hero {}
+export class Hero {
+  readonly tickerItems = [
+    'Available for remote work',
+    'Fullstack Developer',
+    'Based in Brunswick',
+    'Open to work',
+  ];
+
+  readonly tickerGroups = [0, 1, 2];
+  readonly tickerCopies = [0, 1];
+}
