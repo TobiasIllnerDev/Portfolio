73eb0434a0774b4823adc7e6b3d730ff33b2b281
Tobias Illner <140509788+BlueShadow07@users.noreply.github.com>
2026-07-31 11:40:35 +0200

    feat: dialog added

--- src/app/pages/main-section/projects/projects.ts
+++ src/app/pages/main-section/projects/projects.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+
 
 @Component({
   selector: 'app-projects',
@@ -6,4 +7,51 @@ import { Component } from '@angular/core';
   templateUrl: './projects.html',
   styleUrl: './projects.scss',
 })
-export class Projects {}
+
+export class Projects {
+  @ViewChild('projectDialog')
+  projectDialog!: ElementRef<HTMLDialogElement>;
+
+  readonly projects = {
+    pokedex: {
+      number: '01',
+      title: 'Pokedex',
+      description:
+        'A responsive Pokédex that lets users search and explore Pokémon and their most important information.',
+      image: 'images/Pokedex.png',
+      technologies: ['JavaScript', 'HTML', 'CSS'],
+    },
+    sharkie: {
+      number: '02',
+      title: 'Sharkie',
+      description:
+        'A browser-based jump-and-run game built with object-oriented JavaScript.',
+      image: 'images/Sharki-projekt.png',
+      technologies: ['JavaScript', 'HTML', 'CSS'],
+    },
+  };
+
+  selectedProject = this.projects.pokedex;
+
+  openProject(project: keyof typeof this.projects) {
+    this.selectedProject = this.projects[project];
+    this.projectDialog.nativeElement.showModal();
+  }
+
+  closeProject() {
+    this.projectDialog.nativeElement.close();
+  }
+
+  showNextProject() {
+    this.selectedProject =
+      this.selectedProject === this.projects.pokedex
+        ? this.projects.sharkie
+        : this.projects.pokedex;
+  }
+
+  closeOnBackdrop(event: MouseEvent) {
+    if (event.target === this.projectDialog.nativeElement) {
+      this.closeProject();
+    }
+  }
+}
