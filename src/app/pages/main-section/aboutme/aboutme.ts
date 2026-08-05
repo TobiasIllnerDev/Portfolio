fee810d10fb39158d0407c79bd5afe9aecdb1012
Tobias Illner <140509788+BlueShadow07@users.noreply.github.com>
2026-08-05 12:37:04 +0200

    feat: translate about me complet

--- src/app/pages/main-section/aboutme/aboutme.ts
+++ src/app/pages/main-section/aboutme/aboutme.ts
@@ -1,9 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { TranslatePipe, TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-aboutme',
-  imports: [],
+  imports: [TranslatePipe],
   templateUrl: './aboutme.html',
   styleUrl: './aboutme.scss',
 })
-export class Aboutme {}
+export class Aboutme {
+  readonly translate = inject(TranslateService);
+
+  changeLanguage(event: Event): void {
+    const isGerman = (event.target as HTMLInputElement).checked;
+    const language = isGerman ? 'de' : 'en';
+
+    this.translate.use(language);
+    localStorage.setItem('language', language);
+    document.documentElement.lang = language;
+  }
+}
