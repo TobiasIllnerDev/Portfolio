f44e3a4f643c7760209d12304ad8c85103483918
Tobias Illner <140509788+BlueShadow07@users.noreply.github.com>
2026-08-05 13:04:53 +0200

    feat: added translate to skills

--- src/app/pages/main-section/skillset/skillset.ts
+++ src/app/pages/main-section/skillset/skillset.ts
@@ -1,12 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { TranslatePipe, TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-skillset',
-  imports: [],
+  imports: [TranslatePipe],
   templateUrl: './skillset.html',
   styleUrl: './skillset.scss',
 })
 export class Skillset {
+  readonly translate = inject(TranslateService);
+
+  changeLanguage(event: Event): void {
+    const isGerman = (event.target as HTMLInputElement).checked;
+    const language = isGerman ? 'de' : 'en';
+
+    this.translate.use(language);
+    localStorage.setItem('language', language);
+    document.documentElement.lang = language;
+  }
+
   icons = [
     {
       name:'HTML',
