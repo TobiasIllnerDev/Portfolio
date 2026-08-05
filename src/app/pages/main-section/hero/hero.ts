e048e55e108989eca5ef00d4212c8c742ead11c6
Tobias Illner <140509788+BlueShadow07@users.noreply.github.com>
2026-08-05 12:29:11 +0200

    feat: about me section

--- src/app/pages/main-section/hero/hero.ts
+++ src/app/pages/main-section/hero/hero.ts
@@ -1,17 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { TranslatePipe, TranslateService  } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-hero',
-  imports: [],
+  imports: [TranslatePipe],
   templateUrl: './hero.html',
   styleUrl: './hero.scss',
 })
 export class Hero {
+  readonly translate = inject(TranslateService);
+
+  changeLanguage(event: Event): void {
+    const isGerman = (event.target as HTMLInputElement).checked;
+    const language = isGerman ? 'de' : 'en';
+
+    this.translate.use(language);
+    localStorage.setItem('language', language);
+    document.documentElement.lang = language;
+  }
+  
   readonly tickerItems = [
-    'Available for remote work',
-    'Fullstack Developer',
-    'Based in Brunswick',
-    'Open to work',
+    'app.hero.ticker.remote',
+    'app.hero.ticker.developer',
+    'app.hero.ticker.location',
+    'app.hero.ticker.available',
   ];
 
   readonly tickerGroups = [0, 1, 2];
